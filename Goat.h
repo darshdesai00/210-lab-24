61ebc9fef74a52def4876a4bb8fefa02ab40a74a
darshdesai00 <darshdesai50@icloud.com>
2025-10-26 20:55:49 -0700

    Initial commit

---
+++ Goat.h
@@ -0,0 +1,51 @@
+// Goat.h
+
+#ifndef GOAT_H
+#define GOAT_H
+#include <iostream>
+using namespace std;
+
+class Goat {
+private:
+    string name;
+    int age;
+    string color;
+public: 
+    Goat()                          { name = ""; age = 0; color = ""; }
+    // write three more constructors
+
+    //constructor 1: names
+    Goat(string n) {
+         name = n;
+         age = 0;
+         color = "";
+    }
+
+    //constructor 2: names and age
+    Goat(string n, int a) {
+        name = n;
+        age = a;
+        color = "";
+    }
+
+    //constructor 3: name, age and color
+    Goat(string n, int a, string c) {
+        name = n;
+        age = a;
+        color = c;
+    }
+
+
+    // setters and getters
+    void set_name(string n)         { name = n; };
+    string get_name() const         { return name; };
+    void set_age(int a)             { age = a; };
+    int get_age() const             { return age; }
+    void set_color(string c)        { color = c; }
+    string get_color() const        { return color; }
+
+    // write overloaded < operator for the std::list
+    
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <list>
+#include "Goat.h"
+using namespace std;
+
+const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
+
+int select_goat(list<Goat> trip);
+void delete_goat(list<Goat> &trip);
+void add_goat(list<Goat> &trip, string [], string []);
+void display_trip(list<Goat> trip);
+int main_menu();
+
+// menu function
+int main_menu() {
+    int choice = 0;
+
+    do {
+        cout << endl;
+        cout << "*** GOAT MANAGER 3001 ***" << endl;
+        cout << " [1] Add a goat" << endl;
+        cout << " [2] Delete a goat" << endl;
+        cout << " [3] List goats" << endl;
+        cout << " [4] Quit" << endl;
+        cout << " Choice --> ";
+        cin >> choice;
+
+        if (choice < 1 || choice > 4) {
+            cout << "Invalid choice. Try again." << endl;
+        }
+    } while (choice < 1 || choice > 4);
+
+    return choice;   // was missing this brace "checked"
+}                    
+
+void add_goat(list<Goat> &trip, string names[], string colors[]) {
+    // beloe creates a random goat using arrays
+    string n = names[rand() % SZ_NAMES];
+    string c = colors[rand() % SZ_COLORS];
+    int a = rand() % MAX_AGE + 1;
+
+    // creates and add Goat object
+    Goat g(n, a, c);
+    trip.push_back(g);
+
+    cout << n << " (" << c << ", " << a << ") added to the trip!" << endl;
+}
+
+    // below is the display trip funcion
+void display_trip(list<Goat> trip) {
+    if (trip.empty()) {
+        cout << "No goats in the trip right now." << endl;
+        return;
+    }
+
+    cout << "\nCurrent goats in the trip:\n";
+    int i = 1;
+    for (const Goat &g : trip) {
+        cout << "[" << i++ << "] "
+             << g.get_name() << " (" << g.get_color()
+             << ", age " << g.get_age() << ")" << endl;
+    }
+}
+
+// basically lets you choose which goat you want to delete later
+int select_goat(list<Goat> trip) {
+    display_trip(trip);
+    if (trip.empty()) return -1;
+
+    cout << "Enter goat number to delete: ";
+    int num;
+    cin >> num;
+    return num;
+}
+
+// adding delete goat func
+void delete_goat(list<Goat> &trip) {
+    if (trip.empty()) {
+        cout << "No goats to delete." << endl;
+        return;
+    }
+    
+    int num = select_goat(trip);
+    if (num < 1 || num > trip.size()) {
+        cout << "Invalid goat number." << endl;
+        return;
+    }
+
+    auto it = trip.begin();
+    advance(it, num - 1);
+    cout << "Removing " << it->get_name() << "..." << endl;
+    trip.erase(it);
+}
+
+
+// main function
+int main() {
+    srand(time(0));
+    bool again;
+
+    // read & populate arrays for names and colors
+    ifstream fin("names.txt");
+    string names[SZ_NAMES];
+    int i = 0;
+    while (fin >> names[i++]);
+    fin.close();
+
+    ifstream fin1("colors.txt");
+    string colors[SZ_COLORS];
+    i = 0;
+    while (fin1 >> colors[i++]);
+    fin1.close();
+
+    // this is the goat trip list
+    list<Goat> trip;
+    int choice;
+
+    // main menu loop (Replaced cout lines)
+    do {
+        choice = main_menu();
+            switch (choice) {
+        case 1:
+            add_goat(trip, names, colors);
+            break;
+        case 2:
+            delete_goat(trip);
+            break;
+        case 3:
+            display_trip(trip);
+            break;
+        case 4:
+            cout << "Goodbye :)" << endl;
+            break;
+        default:
+            cout << "Invalid choice." << endl;
+    }
+
+           
+    } while (choice != 4);
+
+    return 0;
+}
